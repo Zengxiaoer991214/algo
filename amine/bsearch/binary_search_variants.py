c5464aa464a530e938615d94831e3b93443fc14f
LinLi6 <linli6@kaifa.cn>
2024-08-17 10:48:33 +0800

    [req][bsearch]添加 binary search variants

--- amine/bsearch/binary_search_variants.py
+++ amine/bsearch/binary_search_variants.py
@@ -38,4 +38,49 @@ def binary_search_right(nums: List[int], target: int) -> int:
         return -1
 
 
-def binary_
+def binary_search_left_not_less(nums: List[int], target: int) -> int:
+    low, high = 0, len(nums) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if nums[mid] <= target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    if high >= 0 and nums[high] <= target:
+        return high
+    else:
+        return -1
+
+
+def binary_search_right_not_greater(nums: List[int], target: int) -> int:
+    low, high = 0, len(nums) - 1
+    while low <= high:
+        mid = low + (high - low) // 2
+        if nums[mid] <= target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    if high >= 0 and nums[high] <= target:
+        return high
+    else:
+        return -1
+
+
+if __name__ == "__main__":
+    a = [1, 1, 2, 3, 4, 6, 7, 7, 7, 7, 10, 22]
+
+    print(binary_search_left(a, 0) == -1)
+    print(binary_search_left(a, 7) == 6)
+    print(binary_search_left(a, 30) == -1)
+
+    print(binary_search_right(a, 0) == -1)
+    print(binary_search_right(a, 7) == 9)
+    print(binary_search_right(a, 30) == -1)
+
+    print(binary_search_left_not_less(a, 0) == 0)
+    print(binary_search_left_not_less(a, 5) == 5)
+    print(binary_search_left_not_less(a, 30) == -1)
+
+    print(binary_search_right_not_greater(a, 0) == -1)
+    print(binary_search_right_not_greater(a, 6) == 5)
+    print(binary_search_right_not_greater(a, 30) == 11)
