122d804bedeb242d5c5f86b39e7af2733ad4f8e3
LinLi6 <linli6@kaifa.cn>
2024-08-30 17:40:49 +0800

    【req】【red black tree】 init 2

--- amine/red_and_black_tree/red_and_black_tree.py
+++ amine/red_and_black_tree/red_and_black_tree.py
@@ -1,3 +1,30 @@
-
 class TreeNode:
     def __init__(self, val=None, color=None):
+        self.val = val
+        assert color in ['r', 'b']
+        self.color = 'red' if color == 'r' else 'black'
+
+        self.left = None
+        self.right = None
+        self.parent = None
+
+    def is_black(self):
+        return self.color == 'black'
+
+    def set_black(self):
+        self.color = 'black'
+
+    def set_red(self):
+        self.color = 'red'
+
+
+class RedBlackTree:
+
+    def __init__(self, val_list: None):
+        self.root = None
+        self.black_leaf = TreeNode(color='b')
+
+        if type(val_list) == list:
+            for n in val_list:
+                assert type(n) is int:
+
