54eb71a317b7cd861e3adb1af5f5265d99450677
LinLi6 <linli6@kaifa.cn>
2024-09-06 17:59:13 +0800

    【req】【algo】 红黑树

--- amine/red_and_black_tree/red_and_black_tree.py
+++ amine/red_and_black_tree/red_and_black_tree.py
@@ -1,30 +1,51 @@
-class TreeNode:
-    def __init__(self, val=None, color=None):
-        self.val = val
-        assert color in ['r', 'b']
-        self.color = 'red' if color == 'r' else 'black'
+from enum import Enum
+from typing import Optional, List
 
-        self.left = None
-        self.right = None
-        self.parent = None
+
+class Color(Enum):
+    RED = 1
+    BLACK = 2
+
+
+class TreeNode:
+    def __init__(self, val: int = None, color: Color = None):
+        self.val: int = val
+        self.color = color
+        self.left: Optional['TreeNode'] = None
+        self.right: Optional['TreeNode'] = None
+        self.parent: Optional['TreeNode'] = None
 
     def is_black(self):
-        return self.color == 'black'
+        return self.color == Color.BLACK
 
     def set_black(self):
-        self.color = 'black'
+        self.color = Color.BLACK
 
     def set_red(self):
-        self.color = 'red'
+        self.color = Color.RED
 
 
 class RedBlackTree:
 
-    def __init__(self, val_list: None):
+    def __init__(self, val_list: list[int]):
         self.root = None
-        self.black_leaf = TreeNode(color='b')
+        self.black_leaf = TreeNode(color=Color.RED)
 
         if type(val_list) == list:
             for n in val_list:
-                assert type(n) is int:
+                assert type(n) is int
+                self.insert(n)
 
+    def search(self, val):
+        if self.root is None:
+            return None
+
+        n = self.root
+        while n != self.black_leaf:
+            if val < n.val:
+                n = n.left
+            elif val > n.val:
+                n = n.right
+            else:
+                return n
+        return None
